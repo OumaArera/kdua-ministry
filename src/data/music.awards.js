d450b7105b82ce0c0d974990cebb7f70eea7cc3a
OumaArera <johnouma999@gmail.com>
2025-07-25 16:36:11 +0300

    feat: images

--- src/data/music.awards.js
+++ src/data/music.awards.js
@@ -22,6 +22,119 @@ import sing20 from '../assets/aseda/sing20.jpg';
 import sing21 from '../assets/aseda/sing21.jpg';
 import sing22 from '../assets/aseda/sing22.jpg';
 import sing23 from '../assets/aseda/sing23.jpg';
+import sing24 from '../assets/aseda/sing24.jpg';
+import sing25 from '../assets/aseda/sing25.jpg';
+import sing26 from '../assets/aseda/sing26.jpg';
+import sing27 from '../assets/aseda/sing27.jpg';
+import sing28 from '../assets/aseda/sing28.jpg';
+import sing29 from '../assets/aseda/sing29.jpg';
+import sing30 from '../assets/aseda/sing30.jpg';
+import sing31 from '../assets/aseda/sing31.jpg';
+import sing32 from '../assets/aseda/sing32.jpg';
+import sing33 from '../assets/aseda/sing33.jpg';
+import sing34 from '../assets/aseda/sing34.jpg';
+import sing35 from '../assets/aseda/sing35.jpg';
+import sing36 from '../assets/aseda/sing36.jpg';
+import sing37 from '../assets/aseda/sing37.jpg';
+import sing38 from '../assets/aseda/sing38.jpg';
+import sing39 from '../assets/aseda/sing39.jpg';
+import sing40 from '../assets/aseda/sing40.jpg';
+import sing41 from '../assets/aseda/sing41.jpg';
+import sing42 from '../assets/aseda/sing42.jpg';
+import sing43 from '../assets/aseda/sing43.jpg';
+import sing44 from '../assets/aseda/sing44.jpg';
+import sing45 from '../assets/aseda/sing45.jpg';
+import sing46 from '../assets/aseda/sing46.jpg';
+import sing47 from '../assets/aseda/sing47.jpg';
+import sing48 from '../assets/aseda/sing48.jpg';
+import sing49 from '../assets/aseda/sing49.jpg';
+import sing50 from '../assets/aseda/sing50.jpg';
+import sing51 from '../assets/aseda/sing51.jpg';
+import sing52 from '../assets/aseda/sing52.jpg';
+import sing53 from '../assets/aseda/sing53.jpg';
+import sing54 from '../assets/aseda/sing54.jpg';
+import sing55 from '../assets/aseda/sing55.jpg';
+import sing56 from '../assets/aseda/sing56.jpg';
+import sing57 from '../assets/aseda/sing57.jpg';
+import sing58 from '../assets/aseda/sing58.jpg';
+import sing59 from '../assets/aseda/sing59.jpg';
+import sing60 from '../assets/aseda/sing60.jpg';
+import sing61 from '../assets/aseda/sing61.jpg';
+import sing62 from '../assets/aseda/sing62.jpg';
+import sing63 from '../assets/aseda/sing63.jpg';
+import sing64 from '../assets/aseda/sing64.jpg';
+import sing65 from '../assets/aseda/sing65.jpg';
+import sing66 from '../assets/aseda/sing66.jpg';
+import sing67 from '../assets/aseda/sing67.jpg';
+import sing68 from '../assets/aseda/sing68.jpg';
+import sing69 from '../assets/aseda/sing69.jpg';
+import sing70 from '../assets/aseda/sing70.jpg';
+import sing71 from '../assets/aseda/sing71.jpg';
+import sing72 from '../assets/aseda/sing72.jpg';
+import sing73 from '../assets/aseda/sing73.jpg';
+import sing74 from '../assets/aseda/sing74.jpg';
+import sing75 from '../assets/aseda/sing75.jpg';
+import sing76 from '../assets/aseda/sing76.jpg';
+import sing77 from '../assets/aseda/sing77.jpg';
+import sing78 from '../assets/aseda/sing78.jpg';
+import sing79 from '../assets/aseda/sing79.jpg';
+import sing80 from '../assets/aseda/sing80.jpg';
+import sing81 from '../assets/aseda/sing81.jpg';
+import sing82 from '../assets/aseda/sing82.jpg';
+import sing83 from '../assets/aseda/sing83.jpg';
+import sing84 from '../assets/aseda/sing84.jpg';
+import sing85 from '../assets/aseda/sing85.jpg';
+import sing86 from '../assets/aseda/sing86.jpg';
+import sing87 from '../assets/aseda/sing87.jpg';
+import sing88 from '../assets/aseda/sing88.jpg';
+import sing89 from '../assets/aseda/sing89.jpg';
+import sing90 from '../assets/aseda/sing90.jpg';
+import sing91 from '../assets/aseda/sing91.jpg';
+import sing92 from '../assets/aseda/sing92.jpg';
+import sing93 from '../assets/aseda/sing93.jpg';
+import sing94 from '../assets/aseda/sing94.jpg';
+import sing95 from '../assets/aseda/sing95.jpg';
+import sing96 from '../assets/aseda/sing96.jpg';
+import sing97 from '../assets/aseda/sing97.jpg';
+import sing98 from '../assets/aseda/sing98.jpg';
+import sing99 from '../assets/aseda/sing99.jpg';
+import sing100 from '../assets/aseda/sing100.jpg';
+import sing101 from '../assets/aseda/sing101.jpg';
+import sing102 from '../assets/aseda/sing102.jpg';
+import sing103 from '../assets/aseda/sing103.jpg';
+import sing104 from '../assets/aseda/sing104.jpg';
+import sing105 from '../assets/aseda/sing105.jpg';
+import sing106 from '../assets/aseda/sing106.jpg';
+import sing107 from '../assets/aseda/sing107.jpg';
+import sing108 from '../assets/aseda/sing108.jpg';
+import sing109 from '../assets/aseda/sing109.jpg';
+import sing110 from '../assets/aseda/sing110.jpg';
+import sing111 from '../assets/aseda/sing111.jpg';
+import sing112 from '../assets/aseda/sing112.jpg';
+import sing113 from '../assets/aseda/sing113.jpg';
+import sing114 from '../assets/aseda/sing114.jpg';
+import sing115 from '../assets/aseda/sing115.jpg';
+import sing116 from '../assets/aseda/sing116.jpg';
+import sing117 from '../assets/aseda/sing117.jpg';
+import sing118 from '../assets/aseda/sing118.jpg';
+import sing119 from '../assets/aseda/sing119.jpg';
+import sing120 from '../assets/aseda/sing120.jpg';
+import sing121 from '../assets/aseda/sing121.jpg';
+import sing122 from '../assets/aseda/sing122.jpg';
+import sing123 from '../assets/aseda/sing123.jpg';
+import sing124 from '../assets/aseda/sing124.jpg';
+import sing125 from '../assets/aseda/sing125.jpg';
+import sing126 from '../assets/aseda/sing126.jpg';
+import sing127 from '../assets/aseda/sing127.jpg';
+import sing128 from '../assets/aseda/sing128.jpg';
+import sing129 from '../assets/aseda/sing129.jpg';
+import sing130 from '../assets/aseda/sing130.jpg';
+import sing131 from '../assets/aseda/sing131.jpg';
+import sing132 from '../assets/aseda/sing132.jpg';
+import sing133 from '../assets/aseda/sing133.jpg';
+import sing134 from '../assets/aseda/sing134.jpg';
+import sing135 from '../assets/aseda/sing135.jpg';
+
 // Awards
 import award1 from '../assets/aseda/award1.jpg';
 import award2 from '../assets/aseda/award2.jpg';
@@ -30,6 +143,12 @@ import award4 from '../assets/aseda/award4.jpg';
 import award5 from '../assets/aseda/award5.jpg';
 import award6 from '../assets/aseda/award6.jpg';
 import award7 from '../assets/aseda/award7.jpg';
+import award8 from '../assets/aseda/award8.jpg';
+import award9 from '../assets/aseda/award9.jpg';
+import award10 from '../assets/aseda/award10.jpg';
+import award11 from '../assets/aseda/award11.jpg';
+import award12 from '../assets/aseda/award12.jpg';
+import award13 from '../assets/aseda/award13.jpg';
 // Speakers
 import speaker1 from '../assets/aseda/speaker1.jpg';
 import speaker2 from '../assets/aseda/speaker2.jpg';
@@ -38,6 +157,18 @@ import speaker4 from '../assets/aseda/speaker4.jpg';
 import speaker5 from '../assets/aseda/speaker5.jpg';
 import speaker6 from '../assets/aseda/speaker6.jpg';
 import speaker7 from '../assets/aseda/speaker7.jpg';
+import speaker8 from '../assets/aseda/speaker8.jpg';
+import speaker9 from '../assets/aseda/speaker9.jpg';
+import speaker10 from '../assets/aseda/speaker10.jpg';
+import speaker11 from '../assets/aseda/speaker11.jpg';
+import speaker12 from '../assets/aseda/speaker12.jpg';
+import speaker13 from '../assets/aseda/speaker13.jpg';
+import speaker14 from '../assets/aseda/speaker14.jpg';
+import speaker15 from '../assets/aseda/speaker15.jpg';
+import speaker16 from '../assets/aseda/speaker16.jpg';
+import speaker17 from '../assets/aseda/speaker17.jpg';
+import speaker18 from '../assets/aseda/speaker18.jpg';
+import speaker19 from '../assets/aseda/speaker19.jpg';
 // 2023 events
 import award1_ from '../assets/aseda/23_award1.png';
 import award2_ from '../assets/aseda/23_award2.png';
@@ -52,12 +183,34 @@ export const musicData =[
     {
         event: "Aseda Mucical Concept & Recognition Award",
         year: 2024,
-        speakers: [speaker1, speaker2, speaker3, speaker4, speaker5, speaker6, speaker7],
-        awards: [award1, award2, award3, award4, award5, award6, award7],
+        speakers: [
+            speaker1, speaker2, speaker3, speaker4, speaker5, speaker6, speaker7,
+            speaker8, speaker9, speaker10, speaker11, speaker12, speaker13,
+            speaker14, speaker15, speaker16, speaker17, speaker18, speaker19
+        ],
+        awards: [
+            award1, award2, award3, award4, award5, award6, award7,
+            award8, award9, award10, award11, award12, award13
+        ],
         singers: [
-            sing1, sing2, sing3, sing4, sing5, sing6, sing7, sing8, sing9, sing10,
-            sing11, sing12, sing13, sing14, sing15, sing16, sing17, sing18, sing19, 
-            sing20, sing21, sing22, sing23
+            sing1, sing2, sing3, sing4, sing5, sing6, sing7, sing8, sing9, 
+            sing10, sing11, sing12, sing13, sing14, sing15, sing16, sing17, 
+            sing18, sing19, sing20, sing21, sing22, sing23, sing24, sing25, 
+            sing26, sing27, sing28, sing29, sing30, sing31, sing32, sing33, 
+            sing34, sing35, sing36, sing37, sing38, sing39, sing40, sing41, 
+            sing42, sing43, sing44, sing45, sing46, sing47, sing48, sing49, 
+            sing50, sing51, sing52, sing53, sing54, sing55, sing56, sing57, 
+            sing58, sing59, sing60, sing61, sing62, sing63, sing64, sing65, 
+            sing66, sing67, sing68, sing69, sing70, sing71, sing72, sing73, 
+            sing74, sing75, sing76, sing77, sing78, sing79, sing80, sing81,
+            sing82, sing83, sing84, sing85, sing86, sing87, sing88, sing89, 
+            sing90, sing91, sing92, sing93, sing94, sing95, sing96, sing97, 
+            sing98, sing99, sing100, sing101,sing102, sing103, sing104, 
+            sing105, sing106, sing107, sing108, sing109, sing110, sing111, 
+            sing112, sing113, sing114, sing115, sing116, sing117, sing118, 
+            sing119, sing120, sing121, sing122, sing123, sing124, sing125,
+            sing126, sing127, sing128, sing129, sing130, sing131, sing132, 
+            sing133, sing134, sing135,
         ],
     },
     {
